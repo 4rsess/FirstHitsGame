ed5fc45fb2d62b7ca935dc3d85d63130a39ce94a
4rsess <misha.d.nosov@gmail.com>
2024-06-10 20:43:22 +0700

    первая сцена с историей 90%

--- firstHitsGame/Assets/Scripts/CameraController.cs
+++ firstHitsGame/Assets/Scripts/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
     private Vector3 pos;
-    private float CameraSpeed = 3f;
+    private float CameraSpeed = 4f;
 
     private void Awake()
     {
@@ -18,7 +18,7 @@ public class CameraController : MonoBehaviour
     {
         pos = player.position;
         pos.z = -10f;
-        pos.y += 2f;
+        pos.y += 2.5f;
         pos.x += 3f;
 
         transform.position = Vector3.Lerp(transform.position, pos, CameraSpeed * Time.deltaTime);
