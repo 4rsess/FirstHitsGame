2cc20f4be4f56353f3f2a6f6e7ee1d0a1e2f730e
4rsess <misha.d.nosov@gmail.com>
2024-06-11 23:16:34 +0700

    сделал дэш и присед

--- firstHitsGame/Assets/Scripts/Hero.cs
+++ firstHitsGame/Assets/Scripts/Hero.cs
@@ -8,6 +8,7 @@ public class Hero : AllEntity
     [SerializeField] private float speed = 6f;
     [SerializeField] private int health;
     [SerializeField] private float jumpForce = 10f;
+    private int lungeImpulse = 14;
     private bool isGrounded = false;
 
     [SerializeField] private Image[] hearts;
@@ -17,6 +18,10 @@ public class Hero : AllEntity
 
     public bool isAttacking = false;
     public bool isRecharged = true;
+    public bool isDashing = false;
+    private bool isLungeOnCooldown = false;
+    private bool isCrouching = false;
+
 
     public Transform attackPosition;
     public float attackRange;
@@ -54,18 +59,24 @@ public class Hero : AllEntity
 
     private void Update()
     {
-        if (isGrounded && !isAttacking) State = States.HeroAnimation;
-
-        if (Input.GetButton("Horizontal") && !isAttacking)
+        if (isGrounded && !isAttacking && !isDashing && !isCrouching) 
+            State = States.HeroAnimation;
+        if (Input.GetButton("Horizontal") && !isAttacking && !isDashing)
             Run();
-        if (isGrounded && Input.GetButtonDown("Jump") && !isAttacking)
+        if (isGrounded && Input.GetButtonDown("Jump") && !isAttacking && !isDashing && !isCrouching)
             Jump();
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !isDashing && !isCrouching)
             Attack();
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isDashing && !isLungeOnCooldown && !isCrouching)
+            StartCoroutine(Lunge());
+        if (Input.GetKey(KeyCode.S) && isGrounded && !isDashing && !isAttacking)
+            Crouch();
+        if (Input.GetKeyUp(KeyCode.S))
+            StandUp();
+
 
         if (health > livesCount)
             health = livesCount;
-
         for (int i = 0; i < hearts.Length; i++)
         {
             if (i < health)
@@ -80,9 +91,21 @@ public class Hero : AllEntity
         }
     }
 
+
+    //Move
     private void Run()
     {
-        if (isGrounded) State = States.runAnimation;
+        if (isGrounded)
+        {
+            if (isCrouching)
+            {
+                State = States.crouchAnimation;
+            }
+            else
+            {
+                State = States.runAnimation;
+            }
+        }
 
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
 
@@ -96,12 +119,51 @@ public class Hero : AllEntity
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    private void CheckGround()
+    private IEnumerator Lunge()
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
-        isGrounded = collider.Length > 1;
+        isLungeOnCooldown = true;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
 
-        if (!isGrounded) State = States.jumpAnimation;
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(0, 0); 
+
+        Vector2 lungeDirection = sprite.flipX ? Vector2.left : Vector2.right;
+        float dashSpeed = lungeImpulse / rb.mass;
+        rb.velocity = new Vector2(lungeDirection.x * dashSpeed, 0);
+
+        yield return new WaitForSeconds(0.35f); 
+
+        rb.gravityScale = originalGravity;
+        rb.velocity = new Vector2(0, 0);
+        isDashing = false;
+
+        yield return new WaitForSeconds(3f);
+        isLungeOnCooldown = false;
+    }
+
+    private void Crouch()
+    {
+        if (!isCrouching)
+        {
+            isCrouching = true;
+            State = States.crouchAnimation;
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            collider.size = new Vector2(collider.size.x, collider.size.y / 2);
+            collider.offset = new Vector2(collider.offset.x, collider.offset.y / 2);
+        }
+    }
+
+    private void StandUp()
+    {
+        if (isCrouching)
+        {
+            isCrouching = false;
+            State = States.HeroAnimation;
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            collider.size = new Vector2(collider.size.x, collider.size.y * 2);
+            collider.offset = new Vector2(collider.offset.x, collider.offset.y * 2);
+        }
     }
 
     private void Attack()
@@ -118,6 +180,29 @@ public class Hero : AllEntity
         }
     }
 
+    public override void GetDamage()
+    {
+        health -= 1;
+        Debug.Log(livesCount);
+
+        if (health == 0)
+        {
+            foreach (var h in hearts)
+                h.sprite = deadHeart;
+            Die();
+        }
+    }
+    
+    //end move
+
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
+
+        if (!isGrounded) State = States.jumpAnimation;
+    }
     private void OnAttack()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, enemy);
@@ -127,37 +212,20 @@ public class Hero : AllEntity
             colliders[i].GetComponent<AllEntity>().GetDamage();
         }
     }
-
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPosition.position, attackRange);
     }
-
     private IEnumerator AttackAnimation()
     {
         yield return new WaitForSeconds(0.4f);
         isAttacking = false;
     }
-
     private IEnumerator AttackCoolDown()
     {
         yield return new WaitForSeconds(0.5f);
         isRecharged = true;
-    }
-
-    public override void GetDamage()
-    {
-        health -= 1;
-        Debug.Log(livesCount);
-        if (health == 0)
-        {
-            foreach (var h in hearts)
-                h.sprite = deadHeart;
-            Die();
-        }
-            
-
     }
 
 }
@@ -167,5 +235,6 @@ public enum States
     HeroAnimation,
     runAnimation,
     jumpAnimation,
-    attackAnimation
+    attackAnimation,
+    crouchAnimation
 }
